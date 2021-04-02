fdf45748bca0d832a5c92fe7d042c12fdf5390a7
Ras96 <asymptote.k.k@gmail.com>
2021-04-02 13:29:08 +0900

    fix 範囲をarticle内に制限

--- src/func.js
+++ src/func.js
@@ -74,7 +74,7 @@ exports.findDeadLinks = async () => {
       continue;
     }
     await wait();
-    const links = await page.$$eval('article > a', (list) => list.map((elm) => elm.href));
+    const links = await page.$$eval('article a', (list) => list.map((elm) => elm.href));
     const authors = await page.$$eval('.author-detail > a', (list) => list.map((elm) => elm.href));
     const fetchedLinks = await Promise.all(links.map((link) => fetchLink(link)));
     const dl = fetchedLinks.filter((link) => link !== null);
