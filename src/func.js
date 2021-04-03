178b74de70c1d1402f5a4a9e02d3f739c164ded4
Kira Kawai <asymptote.k.k@gmail.com>
2021-04-04 00:58:50 +0900

    fix

--- src/func.js
+++ src/func.js
@@ -10,9 +10,10 @@ const { Semaphore } = require('await-semaphore');
 const SITEMAP_PAGES_URL = 'https://trap.jp/sitemap-pages.xml';
 const SITEMAP_POSTS_URL = 'https://trap.jp/sitemap-posts.xml';
 const INTERVAL_MS = 5000;
+const SEMAPHORE_COUNT = 3;
 const sitemap = new Sitemapper();
 const fetchSema = new Semaphore(5);
-const crawlSema = new Semaphore(3);
+const crawlSema = new Semaphore(SEMAPHORE_COUNT);
 const pages = [];
 let deadLinks = {};
 
@@ -23,10 +24,8 @@ exports.findDeadLinks = async () => {
   const { sites: posts_urls } = await sitemap.fetch(SITEMAP_POSTS_URL);
   const urls = pages_urls.concat(posts_urls);
   // 2. open a new browser and tabs;
-  const browser = await puppeteer.launch({
-    args: ['--no-sandbox', '--disable-setuid-sandbox'],
-  });
-  for (let i = 0; i < 3; i++) {
+  const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
+  for (let i = 0; i < SEMAPHORE_COUNT; i++) {
     const page = await browser.newPage();
     await page.setRequestInterception(true);
     page.on('request', (r) => {
@@ -46,17 +45,7 @@ exports.findDeadLinks = async () => {
   for (const key of Object.keys(deadLinks)) {
     const links = deadLinks[key].links;
     if (links) {
-      const refetchedLinks = await Promise.all(
-        links.map(async (elm, index) => {
-          // 500番台 or 429
-          if (elm.error.match(/^(5[0-9]{2}|429)/)) {
-            wait(index);
-            console.log(`  Rechecking ${elm.url}`);
-            return await fetchLink(elm.url);
-          }
-          return elm;
-        })
-      );
+      const refetchedLinks = await Promise.all(links.map((elm) => refetchLink(elm)));
       const dl = refetchedLinks.filter((link) => link !== null);
       if (dl.length > 0) {
         deadLinks[key].links = dl;
@@ -75,14 +64,15 @@ const crawlLink = async (url) => {
   const release = await crawlSema.acquire();
   const i = usablePageIndex();
   const page = pages[i].page;
-  console.log(`Checking ${url}`);
+  console.log(`Page${i}: Checking ${url}`);
   try {
     await page.goto(url);
   } catch (err) {
-    console.log(`  Error: ${url} ${err}`);
+    console.log(`Page${i}: Error at ${url}:\n\t${err}`);
     deadLinks[url] = { error: err };
-    release();
     pages[i].using = false;
+    await wait();
+    release();
     return url;
   }
   const links = await page.$$eval('article a', (list) => list.map((elm) => elm.href));
@@ -92,14 +82,14 @@ const crawlLink = async (url) => {
   if (dl.length > 0) {
     deadLinks[url] = { authors, links: dl };
   }
-  release();
   pages[i].using = false;
   await wait();
+  release();
   return url;
 };
 
 const usablePageIndex = () => {
-  for (let i = 0; i < 3; i++) {
+  for (let i = 0; i < SEMAPHORE_COUNT; i++) {
     if (pages[i].using == false) {
       pages[i].using = true;
       return i;
@@ -121,21 +111,34 @@ const fetchLink = async (link) => {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36',
       },
     });
-    release();
     if (fetched.status === 200) {
+      release();
       return null;
     } else {
-      console.log('    Dead Link Found:', link, fetched.status);
+      console.log(`\tDead Link Found: ${link} ${fetched.status}`);
+      release();
       return {
         url: link,
         error: `${fetched.status} ${fetched.statusText}`,
       };
     }
   } catch (err) {
-    console.log('    Error Page Found:', link, err.code);
+    console.log(`\tError Page Found:, ${link} ${err.code}`);
     release();
     return { url: link, error: `${err.name}: ${err.code}` };
   }
+};
+
+const refetchLink = async (elm) => {
+  const release = await fetchSema.acquire();
+  // 500番台 or 429
+  if (elm.error.match(/^(5[0-9]{2}|429)/)) {
+    await wait();
+    console.log(`    Rechecking ${elm.url}`);
+    return await fetchLink(elm.url);
+  }
+  release();
+  return elm;
 };
 
 const wait = async (times = 1) => {
